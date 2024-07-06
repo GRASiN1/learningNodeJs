5a428c9f653454a20cbc5fad5b00d374ae435274
GRASiN1 <gauravrajsingh047@gmail.com>
2024-07-06 21:00:25 +0530

    learned about http status codes

--- project_01/index.js
+++ project_01/index.js
@@ -48,16 +48,20 @@ app.get('/api/users', (req, res) => {
 app.get('/api/user/:id', (req, res) => {
     const id = Number(req.params.id);
     const user = users.find(user => user.id === id);
-    if (!user) return res.json({ status: "User not found" });
+    if (!user) return res.status(400).json({ status: "User not found" });
     return res.json(user);
 })
 
 app.post('/api/user', (req, res) => {
     const body = req.body;
+    if (!body || !body.first_name || !body.last_name || !body.email || !body.gender) {
+        return res.status(400).json({ status: "All fields are required" });
+    }
     users.push({ id: users.length + 1, ...body });
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
         if (err) res.json({ error: `${err}` });
-        else res.json({ status: "User created" });
+        //Status code changed to 201 to tell user created
+        else res.status(201).json({ status: "User created" });
     })
 })
 
